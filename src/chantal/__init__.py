81923e66c9e74dec1d7c0bd0d564de511d9fb1a6
semantic-release <semantic-release>
2026-06-26 06:40:21 +0000

    chore(release): 1.6.0 [skip ci]

--- src/chantal/__init__.py
+++ src/chantal/__init__.py
@@ -8,7 +8,7 @@ Red Hat Subscription authentication, content-addressed storage, and immutable
 snapshots.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __author__ = "Simon Lauger"
 __license__ = "MIT"
 
